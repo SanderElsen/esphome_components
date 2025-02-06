ae4dbc76a47215dfb4752465b3286cb32db34bb2
Sander Elsen <sander.elsen@pandora.be>
2025-02-06 20:19:46 +0100

    .

--- components/ht16k33_7seg/ht16k33_display.cpp
+++ components/ht16k33_7seg/ht16k33_display.cpp
@@ -120,7 +120,7 @@ void HT16K337SegDisplay::print(const char *str) {
       fontc |= 0x4;
       str++;
     }
-    this->buffer_.push_back(0x00);
+    this->buffer_.push_back(0x18);
     this->buffer_.push_back(0xff);
   }
 }
